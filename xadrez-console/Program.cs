c61ac0f274ffafb42e50a1f06cddb8b4c912c720
caio_eng.ambiental@hotmail.com <caio_eng.ambiental@hotmail.com>
2021-08-03 20:09:44 -0300

    testes de movimentacao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,6 +12,20 @@ namespace xadrez_console
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.Tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
                 Tela.imprimirTabuleiro(partida.Tab);
 
                 Console.ReadLine();
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -7,15 +7,18 @@ namespace xadrez
 {
     class PartidaDeXadrez
     {
-        public Tabuleiro Tab { get; private set; }
+        
         private int Turno;
         private Cor JogadorAtual;
+        public Tabuleiro Tab { get; private set; }
+        public bool Terminada { get; private set; }
 
         public PartidaDeXadrez()
         {
             Tab = new Tabuleiro(8, 8);
             Turno = 1;
             JogadorAtual = Cor.Branca;
+            Terminada = false;
             colocarPecas();
         }
 
